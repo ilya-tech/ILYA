77cab450f4b9977e34992ab2bf481aff264b366d
Yoann <41453809+YoannMourot@users.noreply.github.com>
2019-04-08 18:58:17 +0200

    ajouts page vision
    - ajouts des box verticales
    - mise en place de bootstrap pour le responsive de l'iframe de la vidéo youtube

--- vision.php
+++ vision.php
@@ -3,21 +3,55 @@
   <head>
     <?php include('included_contents/head.html'); ?>
   </head>
-  
+
   <body>
     <?php include('included_contents/navbar.html'); ?>
 
 
-    <section class="videopresentation">
+    <section id="videopresentation">
         <h1>Notre vision</h1>
       <div class="maincontainer">
-        <div class="video-container">
-          <div>
-            <iframe src="https://www.youtube.com/embed/pLNhJOgLoFc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <div class="embed-responsive embed-responsive-16by9">
+          <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+        </div>
+      </div>
+    </section>
+
+    <section id="boxpresentation">
+      <div class="maincontainer">
+        <div class="row">
+          <div class="col-lg-4 col-xs-12">
+            <div class="image_container">
+              <img src="images/verticalbox_image_test" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-lg-4 col-xs-12">
+            <div class="image_container">
+              <img src="images/vision" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="col-lg-4 col-xs-12">
+            <div class="image_container">
+              <img src="images/verticalbox_image_test" alt="">
+            </div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
       </div>
     </section>
+
+    <section id="participe">
+      <h1>participe</h1>
+    </section>
+
+
+    <footer>
+      <div class="footertext">
+        <a href="#">Mention légales </a><small>© ILYA Tous droits réservés</small>
+      </div>
+    </footer>
 
 
     <?php include('included_contents/allscripts.html') ?>  </body>
